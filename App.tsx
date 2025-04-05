c77d36fc242c9ef45b4afef12690031094d5650a
lucasgfaj <lucasgfajardo09@gmail.com>
2025-04-05 16:11:09 -0300

    OK: Login Component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Car from './src/components/Car';
+import Login from './src/components/Login';
 
-let obj = {
-  car: 'Fuscao Preto',
-  brand: 'VW',
-  Hp: 150
- }
  
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Car car="Fusca" brand="VW" hp={150}/>
-
+      <Login />
       <StatusBar style="auto" />
     </View>
   );
@@ -22,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Login.tsx
@@ -0,0 +1,44 @@
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+
+export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        console.log("Username", username);
+        console.log("Password", password);
+
+    }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput style={styles.input} placeholder='Username' value={username} onChangeText={setUsername} />
+      <TextInput style={styles.input} placeholder='Password' value={password} onChangeText={setPassword} secureTextEntry={true} />
+        <Button title='Login' onPress={handleLogin} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 8,
+        borderRadius: 4,
+    },
+});
